9f307b4eca2d0835d7a02141e6e6b43d7e3aea1a
Pedro Lima <pedrolimart@gmail.com>
2023-10-20 17:25:02 -0300

    1.5

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 function sim(){
 
     alert("Parabéns por ter aceitado meu pedido!");
+    levaIG();
 }
 
 function desvia(){
-   
- 
    var btn = document.getElementById("btn2");
    
    console.log("Desviei");
-//    console.log(btn);
-
- 
-
    btn.style.position = "absolute"; 
    btn.style.bottom=geraPosicao(9,80);
    btn.style.left=geraPosicao(9,80);
@@ -22,4 +17,39 @@ function desvia(){
 }
 function geraPosicao(min, max) {
     return (Math.random() * (max - min) + min) +"%";
+}
+
+function validaForm(){
+
+    var p1 = document.getElementById();
+    var p2 = document.getElementById();
+    var p3 = document.getElementById();
+    var p4 = document.getElementById();
+    
+    //refresh da pagina e envio
+}
+
+function Contador(){
+
+}
+
+function levaIG(){
+
+    var btn=document.getElementById("btn1");
+    
+    var contNamoro=document.getElementById("blocoPedidoNamoro");
+
+    contNamoro.style.display="none";
+    contNamoro.parentNode.removeChild(contNamoro);
+
+
+    //vai para o IG
+
+
+    //contNamoro.style.transitionDuration=
+    
+    //setTimeout();
+
+    //<p>Boao de enviar pedido ao instagram</p>
+    //<a href="https://www.instagram.com/limaapedro"> Clique e seja feliz x)</a>
 }
